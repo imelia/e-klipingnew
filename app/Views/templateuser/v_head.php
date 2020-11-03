9bef9053775d8b5a41359e35f13606ba8fbbcea3
RuryAgista <57553331+RuryAgista@users.noreply.github.com>
2020-11-02 16:39:11 -0800

    template

---
+++ app/Views/templateuser/v_head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>NewsFeed</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/animate.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/font.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/li-scroller.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/slick.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/jquery.fancybox.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/theme.css">
+<link rel="stylesheet" type="text/css" href="assetsuser/css/style.css">
+<!--[if lt IE 9]>
+<script src="assets/js/html5shiv.min.js"></script>
+<script src="assets/js/respond.min.js"></script>
+<![endif]-->
+</head>
---
+++ app/Views/templateuser/v_header.php
@@ -0,0 +1,30 @@
+<body>
+<div id="preloader">
+  <div id="status">&nbsp;</div>
+</div>
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+<div class="container">
+  <header id="header">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="header_top">
+          <div class="header_top_left">
+            <ul class="top_nav">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="pages/contact.html">Contact</a></li>
+            </ul>
+          </div>
+          <div class="header_top_right">
+            <p><?php echo date('D - M - Y | (H:m:i)');?></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="header_bottom">
+          <div class="logo_area"><a href="index.html" class="logo"><img src="assetsuser/images/logo.jpg" alt=""></a></div>
+          <div class="add_banner"><a href="#"><img src="assetsuser/images/addbanner_728x90_V1.jpg" alt=""></a></div>
+        </div>
+      </div>
+    </div>
+  </header>
