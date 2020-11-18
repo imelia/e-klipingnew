67b617521cce50fe766e2a2de67146a9469e1bc8
RuryAgista <57553331+RuryAgista@users.noreply.github.com>
2020-11-18 20:42:29 +0700

    HOREE

--- app/Views/templatenew/v_wrapper.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    echo view('templatenew/v_head');
-    echo view('templatenew/v_header');
-    echo view('templatenew/v_content');
-    echo view('templatenew/v_footer');
-    
--- app/Views/templatenew/v_wrapper2.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo view('templatenew/v_head');
-    echo view('templatenew/v_header');
-    echo view('templatenew/v_content2');
-    echo view('templatenew/v_footer');
